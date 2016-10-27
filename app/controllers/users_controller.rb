00aed6c12bde2905fa29a04f2524697b3113660c
Sachin Srinivasan <ssampathkumar@qwinix.io>
2016-10-27 17:05:49 +0530

    railstest

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+    def index
+    	@users = User.all
+    end
+
+    def create
+		@user = User.new(user_params)
+		@user.save
+		redirect_to user_list_path
+		end	
+
+    def edit
+    @user = User.find_by_id(params[:id])
+    end 
+
+    def update
+        @user = User.find_by_id(params[:id])
+        @user.update_attributes(user_params)
+        redirect_to user_list_path
+    end
+
+    def destroy
+    	@user = User.find_by_id(params[:id])
+    	@user.destroy
+    	redirect_to user_list_path
+    end
+
+    def show
+    	@user = User.find_by_id(params[:id])
+    end 
+    
+	private
+    def user_params
+	    params.require(:user).permit(:firstname,:secondname,:email,:password)
+        end
+    end
+
+
+
+
+
